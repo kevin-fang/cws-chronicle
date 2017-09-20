1cc2a64451de006d544634c52485adea845fdbb6
Kevin Fang <kfang1233@gmail.com>
2017-09-20 18:57:04 -0400

    Set up backend to deliver texts

--- backend/articles.js
+++ backend/articles.js
@@ -1,14 +1,40 @@
 var fs = require('fs')
+var path = require('path')
 
-function getArticles(directory) {
+function getArticleList(directory) {
 	return new Promise((resolve, reject) => {
 		fs.readdir(directory, (err, items) => {
 			if (err) reject(err)
-			resolve(items)
+			resolve(
+				items.map(item => {
+					return item.split('.')
+				}).map(item => {
+					return {title: item[0].split('-').join(" "), extension: item[1]}
+				}).reduce((list, item) => {
+					return list.concat(item)
+				}, new Array()))
+		})
+	})
+}
+
+function getArticle(filename, directory) {
+	console.log(filename + ", " + directory)
+	return new Promise((resolve, reject) => {
+		var file = path.join(directory, filename)
+		fs.readFile(file, (err, content) => {
+			var buffer = "";
+			if (err) {
+				reject(err)
+			} else {
+				buffer += content
+			}
+			resolve(buffer)
+
 		})
 	})
 }
 
 module.exports = {
-	getArticles: getArticles
+	getArticleList: getArticleList,
+	getArticle: getArticle
 }
--- backend/server.js
+++ backend/server.js
@@ -3,20 +3,33 @@ var app = express()
 var config = require('./config.json')
 var article = require('./articles.js')
 
-app.get('/', (res, req) => {
+app.get('/', (req, res) => {
 	console.log(config.article_directory)
-	article.getArticles(config.article_directory)
-		.then(items => {
-			console.log(items)
-		}).catch(err => {
-			console.log(err)
+	res.setHeader('Content-Type', 'text/json')
+	article.getArticleList(config.article_directory)
+		.then(articles => {
+			res.send(JSON.stringify(articles))
+		}).catch(console.log)
+})
+
+app.get('/article', (req, res) => {
+	var filename = req.query.file
+
+	res.setHeader('Content-Type', 'text/html')
+	article.getArticle(filename, config.article_directory)
+		.then(content => {
+			res.send(content)
 		})
+		.catch(console.log)
 })
 
 const startServer = () => {
 	app.listen(config.port)
 	console.log("Server listening on port " + config.port)
 	
+	article.getArticleList(config.article_directory)
+		.then(console.log)
+		.catch(console.log)
 }
 
 startServer()
